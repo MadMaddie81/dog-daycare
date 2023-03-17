a4edbb3490635ab5b3e547508021cc8b6629f745
Madeleine Vikner <m_vikner@live.se>
2023-03-17 10:32:32 +0000

    Fix heroku app crash

--- preciouspaws/settings.py
+++ preciouspaws/settings.py
@@ -145,4 +145,3 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-codestar
