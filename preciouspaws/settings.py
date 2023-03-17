50517ad680966c4be0d39fe5f53a668927e80725
Madeleine Vikner <m_vikner@live.se>
2023-03-17 10:16:09 +0000

    Fix heroku app crash

--- preciouspaws/settings.py
+++ preciouspaws/settings.py
@@ -30,12 +30,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = development
+DEBUG = True
 
-if development:
-    ALLOWED_HOSTS = ['localhost']
-else:
-    ALLOWED_HOSTS = ['precious-paws.herokuapp.com/']
+
+ALLOWED_HOSTS = ['localhost', 'precious-paws.herokuapp.com/']
 
 
 # Application definition
@@ -86,17 +84,17 @@ WSGI_APPLICATION = 'preciouspaws.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# if development:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': BASE_DIR / 'db.sqlite3',
-#         }
-#     }
-# else:
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+if development:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+        }
 
 
 # Password validation
