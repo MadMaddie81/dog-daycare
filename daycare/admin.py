72437e854470805080a23ec9ba8c2094ba113201
Madeleine Vikner <m_vikner@live.se>
2023-03-21 15:33:19 +0000

    Create model for application

--- daycare/admin.py
+++ daycare/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
-from .models import Service
+from .models import Service, Application
 
 admin.site.register(Service)
+
+
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+
+    prepopulated_fields = {'slug': ('dog_name',)}
+    list_display = ('dog_name', 'slug', 'status', 'created_on')
--- daycare/models.py
+++ daycare/models.py
@@ -15,3 +15,71 @@ class Service(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Application(models.Model):
+
+    STATUS = [
+        (0, 'Pending'),
+        (1, 'Approved'),
+        (3, 'Rejected')
+    ]
+
+    WEIGHT_CHOICES = [
+        (None, 'Pick a choice'),
+        ('Mini', 'Mini (< 2 kg)'),
+        ('Small', 'Small (2-8 kg)'),
+        ('Medium', 'Medium (8-15 kg)'),
+        ('Large', 'Large (> 15 kg)')
+    ]
+
+    HEIGHT_CHOICES = [
+        (None, 'Pick a choice'),
+        ('Mini', 'Mini (< 25 cm)'),
+        ('Small', 'Small (25-36 cm)'),
+        ('Medium', 'Medium (36-45 cm)'),
+        ('Large', 'Large (> 45 cm)')
+    ]
+
+    GENDER_CHOICES = [
+        (None, 'Pick a choice'),
+        ('Male', 'Male'),
+        ('Female', 'Female')
+    ]
+
+    dog_name = models.CharField(max_length=200, blank=False)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, default='', on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    breed = models.CharField(max_length=200, blank=False, default='')
+    weight = models.CharField(
+        max_length=10,
+        choices=WEIGHT_CHOICES,
+        default=None
+        )
+    height = models.CharField(
+        max_length=10,
+        choices=HEIGHT_CHOICES,
+        default=None
+        )
+    gender = models.CharField(
+        max_length=10,
+        choices=GENDER_CHOICES,
+        default=None
+        )
+    neutered = models.BooleanField(default=False)
+    insured = models.BooleanField(default=False)
+    vaccinated = models.BooleanField(default=False)
+    experience = models.BooleanField(default=False)
+    info = models.TextField()
+    owner_first_name = models.CharField(max_length=200, blank=False)
+    owner_last_name = models.CharField(max_length=200, blank=False)
+    email = models.EmailField(blank=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return self.slug
